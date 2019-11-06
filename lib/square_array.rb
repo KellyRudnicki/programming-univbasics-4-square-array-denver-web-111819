6a9281f04ea18b4c31e5b4158aa1579e9321295c
Kelly Rudnicki <kelly.rudnicki@comcast.net>
2019-11-06 22:28:40 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,12 @@
 def square_array(array)
-  # your code here
+  
+  new_array = Array.new(array.length)
+  i = 0 
+  
+  while i < array.length do
+    new_array[i] = array[i] ** 2
+    i += 1
+  end 
+  
+  new_array
 end
