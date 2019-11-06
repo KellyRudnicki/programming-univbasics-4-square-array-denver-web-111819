fc865cf95290dd1bc01a33f7d79f2c85454326f6
Kelly Rudnicki <kelly.rudnicki@comcast.net>
2019-11-06 21:41:45 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,8 +2,8 @@ def square_array(array)
   numbers = []
   counter = 0 
   
-  while numbers[counter] do
-    puts numbers[counter] ** 2
+  while counter < numbers.length do
+    numbers[counter] ** 2
     counter += 1
   end 
 end
